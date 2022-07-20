090b9c234500218c91e02f7877b535758522ee75
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-07-20 14:00:26 -0300

    resolvendo erros do lint do requisito 7 e 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,12 +19,11 @@ const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counterTwo = 2; counterTwo <= 3; counterTwo += 1) {
+      myArray.push(counterTwo);
     }
   }
   return myArray;
 };
-console.log(myCounter())
 
 module.exports = myCounter;
